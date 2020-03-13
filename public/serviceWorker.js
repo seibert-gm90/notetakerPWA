2b5c56da3172e6807e0f6ba1c08a4366b18664bf
Gabby Jones <jones.gm90@gmail.com>
2020-03-13 11:12:37 -0400

    getting service worker working

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,18 +1,8 @@
 const FILES_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/favicon.ico',
-    '/manifest.webmanifest',
-    '/assets/css/style.css',
-    '/assets/js/loadImages.js',
-    '/assets/images/icons/icon-72x72.png',
-    '/assets/images/icons/icon-96x96.png',
-    '/assets/images/icons/icon-128x128.png',
-    '/assets/images/icons/icon-144x144.png',
-    '/assets/images/icons/icon-152x152.png',
-    '/assets/images/icons/icon-192x192.png',
-    '/assets/images/icons/icon-384x384.png',
-    '/assets/images/icons/icon-512x512.png',
+    "/",
+    "/index.html",
+    "app.js",
+     "favicon.ico"
   ];
   
   const CACHE_NAME = "static-cache-v2";
